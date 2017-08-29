a14a71a4e6acd8061c5295240faf4d0a6b973ca3
Marcello Nycz <nolexo@o2.pl>
2017-08-29 22:20:52 +0200

    dodana granica nerwow

--- world.py
+++ world.py
@@ -1,1 +1,12 @@
-print 'hello world'
+cierpliwosc = raw_input("jaka jest twoja cieprliwosc? w skali 1-100")
+if (cierpliwosc.isdigit()):
+    if (int(cierpliwosc) > 100):
+        print 'nie zdechniesz szybko'
+    else:
+        print 'GL & HF'
+else:
+    print "czytac nie potrafisz?"
+
+
+
+
